662526d19cce7b550277c6a2fd39816c4ae97d1b
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-11-21 00:35:32 +0600

    Time: 11 ms (57.49%) | Memory: 9.3 MB (68.19%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,25 +1,34 @@
 /*
-find the element whose count is less than k
-that character surely won't be included in the answer
-so we have to solve it for the left and right string without including that character and return the maximum length. 
+Apply sliding window. Main technique of sliding window is to extend and shrink the window based on some condition.
+Window will be extended until the end of the string. How should we shrink the window?
+The trick used here is that we allow a fixed number of unique characters in the window. If the unique characters in the window exceeds that fixed number then we shrink the window. We do this for 1..26 
 */
 class Solution {
 public:
+    bool check(vector<int> &count , int k){
+        int mx = INT_MAX;
+        for(int i : count)
+            if(i)
+                mx = min(i , mx);
+        return mx >= k;
+    }
     int longestSubstring(string s, int k) {
-        if(s.size() == 0 or k > s.size())  
-            return 0;
-        if(k == 0)
-            return s.size();
-        vector<int> count(26);
-        for(char c : s)
-            count[c - 'a']++;
-        int n = s.size() , i = 0;
-        while(i < n and count[s[i] - 'a'] >= k)
-            i++;
-        if(i == n)
-            return n;
-        int left = longestSubstring(s.substr(0 , i) , k);
-        int right = longestSubstring(s.substr(i + 1) , k);
-        return max(left , right);
+        int maxChar = set<char> (s.begin() , s.end()).size() , ans = 0 , n = s.size();
+        for(int i = 1 ; i <= maxChar ; i++){
+            unordered_set <char> currChar;
+            vector<int> count(26);
+            for(int left = 0 , right = 0 ; right < n ; right++){
+                currChar.insert(s[right]);
+                count[s[right] - 'a']++;
+                while(currChar.size() > i){ // shrink the window
+                    char c = s[left++];
+                    if(--count[c - 'a'] == 0)
+                        currChar.erase(c);
+                }
+                if(check(count , k))
+                    ans = max(ans , right - left + 1);
+            }   
+        }
+        return ans;
     }
 };
