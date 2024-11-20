6c6bfae48c20bb79c198496e69f52c3aec095033
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-11-21 01:59:11 +0600

    Time: 182 ms (49.52%) | Memory: 131.1 MB (14.28%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,17 +1,16 @@
-/*
-Maintain a set to keep track of number unique elements and a window for sum. If the current number creates duplicacy erase from set until it doesn't (it's the only element) and don't forget to update the window sum.
-*/
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int ans = 0 , n = nums.size() , window = 0;
-        unordered_set <int> count;
+        unordered_map <int , int> count;
         for(int left = 0 , right = 0 ; right < n ; right++){
-            while(count.contains(nums[right])){
-                count.erase(nums[left]);
-                window -= nums[left++];
+            if(count.contains(nums[right])){
+                while(left <= count[nums[right]]){
+                    count.erase(nums[left]);
+                    window -= nums[left++];
+                }
             }
-            count.insert(nums[right]);
+            count[nums[right]] = right;
             window += nums[right];
             ans = max(ans , window);
         }
