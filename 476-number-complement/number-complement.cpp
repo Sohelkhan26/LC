02af704a7151b42c889893c79dcefeec008c6f7e
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-11-30 23:59:51 +0600

    Time: 0 ms (100.00%) | Memory: 7.9 MB (7.53%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = ~0;
+        while(num & mask) mask <<= 1;
+        return ~num ^ mask;
+    }
+};
+
+/*
+প্রথম দেখায় মনে হতে পারে এনসার হবে ~num আসলে এনসার এটাই কিন্তু num এর leading zero গুলা ঝামেলা করে। তাই ~৫ = -৬ হয়ে যায়। তাহলে leading zero গুলা eliminate করতে হবে। while(num & mask) mask <<= 1
+এই কাজটাই করে। যতক্ষন পর্যন্ত mask এর যেকোন সেট বিট num এর কোন বিট এর সাথে result উতপন্ন করবে ততক্ষন Left Shift করতে হবে। তাহলে লাস্ট সেট বিটের পরে mask এর সব ১১১১ থাকবে। ~num  হলো আসল এনসার 
+এটার সাথে ^ mask করে leading zero eliminate করা হইছে। 
+num          = 00000101
+mask         = 11111000 (After while loop)
+~mask & ~num = 00000010
+*/
