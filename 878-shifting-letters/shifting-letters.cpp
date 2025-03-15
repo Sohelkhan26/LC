7acbb4968711313861e129232503e86244f1ceec
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-15 23:02:37 +0600

    Time: 4 ms (61.65%) | Memory: 71.7 MB (57.91%) - LeetSync

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
-        int n = s.size();
-        for(int i = n - 2 ; i >= 0 ; i--)
-            shifts[i] = (shifts[i + 1] + shifts[i]) % 26;
-        for(int i = 0 ; i < s.size() ; i++){
-            int a = s[i] - 'a';
-            a = (a + shifts[i]) % 26;
-            s[i] = a + 'a';
-        }
+        for(int i = s.size() - 1 , prev = 0 ; i >= 0 ; prev %= 26 , i--)
+            s[i] = ((s[i] - 'a') + (prev += shifts[i])) % 26 + 'a';
         return s;
     }
 };
