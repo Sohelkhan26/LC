1508e38b6fead7dd929042909315d352cf53aefc
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-16 17:38:33 +0600

    Time: 4 ms (26.23%) | Memory: 12 MB (25.82%) - LeetSync

--- 827-expressive-words/expressive-words.cpp
+++ 827-expressive-words/expressive-words.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector <pair<char,int>> compress(string s){
+    vector <pair<char,int>> compress(string &s){
         int cnt = 1 , n = s.size();
         vector <pair<char,int>> S;
         for(int i = 1 ; i < s.size() ; i++){
@@ -15,7 +15,7 @@ public:
         return S;
     }
     vector <pair<char,int>> a;
-    bool solve(string c){
+    bool solve(string &c){
         auto b = compress(c);
         if(a.size() != b.size())
             return false;
@@ -31,11 +31,15 @@ public:
     }
     int expressiveWords(string s, vector<string>& words) {
         a = compress(s);
-        for(auto it : a)
-            cout << it.first << ' ' << it.second << endl;
         int ans = 0;
         for(string c : words)
             ans += solve(c);
         return ans;
     }
 };
+
+/*
+In every string in words, some characters can be inserted so that size of the group with same character is at least 3.
+So, for every character group we have to check if it's count is less than or equal to corresponding group in s(if the group size is greater than 3).
+If the group size is less than 3, then group in s and words[i] should be of same count.
+*/
