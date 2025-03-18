0bae2ea4df8f685ff3f9cda0355487d8e7f4c47e
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-19 01:35:51 +0600

    Time: 71 ms (36.57%) | Memory: 15.9 MB (36.29%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int largestOverlap(vector<vector<int>>& a, vector<vector<int>>& b) {
-        int n = a.size() , ans = 0 , overlap = 0;
-        for(int xOffset = - n + 1 ; xOffset < n ; xOffset++){
-            for(int yOffset = - n + 1 ; yOffset < n ; yOffset++){
-                overlap = 0;
-                for(int i = 0 ; i < n ; i++){
-                    for(int j = 0 ; j < n ; j++){
-                        int newI = i + xOffset , newJ = j + yOffset;
-                        if(min(newI , newJ) >= 0 and newI < n and newJ < n)
-                            overlap += (a[i][j] == b[newI][newJ] and a[i][j]);
-                    }
-                    ans = max(ans , overlap);
-                }
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size() , ans = 0;
+        vector <pair<int,int>> a , b;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(img1[i][j])
+                    a.push_back({i , j});
+                if(img2[i][j])
+                    b.push_back({i , j});
             }
         }
+        map <pair<int,int>,int> freq;
+        for(auto [x1 , y1] : a)
+            for(auto [x2 , y2] : b){
+                int diffX = x1 - x2 , diffY = y1 - y2;
+                ans = max(ans , ++freq[{diffX , diffY}]);
+            }
         return ans;
     }
 };
+
+/*
+Map Every 1 from img1 to img2. 
+freq[{x,y}] map counts how many 1 overlap with the exact same relative change {x , y} in position.
+*/
