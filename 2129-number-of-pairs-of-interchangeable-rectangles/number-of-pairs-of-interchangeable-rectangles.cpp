4489c51e6718805af77925585c09bd74770e154b
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-19 01:47:14 +0600

    Time: 188 ms (15.20%) | Memory: 158.9 MB (26.32%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     long long interchangeableRectangles(vector<vector<int>>& rectangles) {
-        unordered_map <double,int> freq;
+        map <pair<int,int>,int> freq;
         long long ans = 0;
         for(auto it : rectangles){
-            double w = it[0] , h = it[1];
-            ans += freq[w / h]++;
+            int w = it[0] , h = it[1];
+            int div = __gcd(w , h);
+            ans += freq[{w / div , h / div}]++;
         }
         return ans;
     }
 };
+
+/*
+Using double data type compromises precision.
+To find out the ratio, numerator and denominator both are devided by __gcd(w , h).
+Instead of storing the ratio directly in the map , store these instead for beater precision.
+*/
