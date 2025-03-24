1106ccd47e558bb77e29a3dc78282c2c0efeec43
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-24 17:28:17 +0600

    Time: 17 ms (37.50%) | Memory: 41.5 MB (30.96%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -3,17 +3,11 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& e) {
         int n = maze.size() , m = maze[0].size();
         queue <pair<int,int>> q;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if((i == 0 or i == n - 1 or j == 0 or j == m - 1 ) and maze[i][j] == '.'){
-                    if(i == e[0] and j == e[1])
-                        continue;
-                    cout << i << ' ' << j << endl;
-                    q.push({i , j});
-                    maze[i][j] = '+';
-                }
-            }
-        }
+        for(int i = 0 ; i < n ; i++)
+            for(int j = 0 ; j < m ; j++)
+                if(maze[i][j] == '.' and not (i == e[0] and j == e[1]))
+                    if(i == 0 or i == n - 1 or j == 0 or j == m - 1)
+                        q.push({i , j}) , maze[i][j] = '+';
         int path = 0;
         while(not q.empty()){
             int s = q.size();
@@ -21,13 +15,10 @@ public:
                 auto [x , y] = q.front(); q.pop();
                 if(x == e[0] and y == e[1])
                     return path;
-                maze[x][y] = '+';
                 for(auto [dx , dy] : vector<pair<int,int>> {{1 , 0} , {0 , 1} , {-1 , 0} , {0 , -1}}){
                     int newX = x + dx , newY = y + dy;
-                    if(min(newX , newY) >= 0 and newX < n and newY < m and maze[newX][newY] != '+'){
-                        maze[newX][newY] = '+';
-                        q.push({newX , newY});
-                    }
+                    if(min(newX , newY) >= 0 and newX < n and newY < m and maze[newX][newY] != '+')
+                        q.push({newX , newY}) , maze[newX][newY] = '+';
                 }
             }
             path++;
@@ -35,3 +26,7 @@ public:
         return -1;
     }
 };
+
+/*
+Use multisource BFS. Push all border open cells and start traversing. If the entrance is encountered, return the path.
+*/
