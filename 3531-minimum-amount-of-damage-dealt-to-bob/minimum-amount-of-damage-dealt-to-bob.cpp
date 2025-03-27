bf4480deb93f3c99dd25ce8117734932ecb7787f
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-27 23:03:33 +0600

    Time: 64 ms (58.54%) | Memory: 148.5 MB (37.20%) - LeetSync

--- 3531-minimum-amount-of-damage-dealt-to-bob/minimum-amount-of-damage-dealt-to-bob.cpp
+++ 3531-minimum-amount-of-damage-dealt-to-bob/minimum-amount-of-damage-dealt-to-bob.cpp
@@ -3,19 +3,20 @@ public:
     typedef pair<int,int> p;
     long long minDamage(int power, vector<int>& damage, vector<int>& health) {
         long long ans = 0 , n = damage.size() , total = 0;
-        priority_queue <vector<double>> pq;
+        vector<pair<int,int>> v;
         for(int i = 0 ; i < n ; i++){
+            int time = ceil(health[i] / (power * 1.0));
             total += damage[i];
-            double time = ceil(health[i] / (power * 1.0));
-            double maxDamage = damage[i] / time * 1.0;
-            pq.push({maxDamage , (double) damage[i] , time});
+            v.push_back({damage[i] , time});
         }
-        while(not pq.empty()){
-            auto it = pq.top(); pq.pop();
-            int d = it[1] , t = it[2];
-            ans += (total * t);
-            total -= d;
-        }
+        sort(v.begin() , v.end() , [&](auto a , auto b){
+            return (double)a.first / a.second > (double)b.first / b.second;
+        });
+        for(auto [d , t] : v)
+            ans += (total * t) , total -= d;
         return ans;
     }
 };
+/*
+Be greedy, take out the enemy that deals most damage per unit of time.
+*/
