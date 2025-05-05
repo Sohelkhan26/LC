8f95e415f495d74cb7ade5aa4f4fd3c3f65b07da
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-05 22:37:14 +0600

    Time: 2 ms (41.73%) | Memory: 79.5 MB (7.19%) - LeetSync

--- 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.cpp
+++ 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.cpp
@@ -4,14 +4,17 @@ public:
     long long numberOfWeeks(vector<int>& nums) {
         long long total = accumulate(all(nums) , 0ll) , mx = *max_element(all(nums)) , 
         rest = total - mx;
-        if(mx > rest)
+        if(mx > total / 2)
             return 2 * rest + 1;
         return total;
     }
 };
 
 /*
-Requires huge level of observation and intuition. 
-Observation is: no matter what the parity and numbers are, unless one milestone is greater than all other milestones combined, then all the milestones can be completed.
-If maxElement is greater, then all lower milestones can be completed by interleaving maxElement in between. And 1 from maxElement again at the end.
+Think like this: There n items, nums[i] represents the count of i'th item. 
+Place them in such manner so that no adjacent items is same. Longest such sequence is the desired answer.
+Say there are m indices to place the items. m = total = sum(nums) 
+Be greedy, highest count item is placed at odd or even index and other items should be in between. If one item has high frequency , > m / 2 then some of them are bound to be adjacent. There sum of other items is "rest" , then there are "rest + 1" holes that can be filled by the highest frequency item. Highest frequencey item can be placed in between and each item is counted so 2 * rest(because of problem statement).
+
+If there is no such high frequency item, then all of them can be placed non-adjacently beause there is no restriction on the length "m".
 */
