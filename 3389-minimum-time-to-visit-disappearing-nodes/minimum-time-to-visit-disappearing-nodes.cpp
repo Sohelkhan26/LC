86295db00bf5d8bac2798abc7022be7d10ca4471
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-07 00:58:24 +0600

    Time: 220 ms (42.22%) | Memory: 275.5 MB (25.56%) - LeetSync

--- 3389-minimum-time-to-visit-disappearing-nodes/minimum-time-to-visit-disappearing-nodes.cpp
+++ 3389-minimum-time-to-visit-disappearing-nodes/minimum-time-to-visit-disappearing-nodes.cpp
@@ -1,45 +1,33 @@
-typedef long long ll;
-typedef pair<ll , ll> P;
-vector<int> Dijktra(vector<vector<int>> &edges , int source , int n , vector <int> &disappear){
-    ll from , distance;
-    vector<vector<P>> adj(n);
-    vector <int> dist(n , INT_MAX);
-    for(auto it : edges){
-        int from = it[0] , to = it[1] , weight = it[2];
-        adj[from].push_back({weight , to});
-        adj[to].push_back({weight , from});
-    }
-    priority_queue <P , vector <P> , greater<P>> pq;
-    dist[source] = 0;
-    pq.push({0 , source});
-    while(not pq.empty()){
-        from = pq.top().second , distance = pq.top().first;
-        pq.pop();
-        if(distance > dist[from])continue;
-        for(auto it : adj[from]){
-            ll to = it.second , distance = it.first;
-            distance += dist[from];
-            if(distance >= dist[to] or distance >= disappear[to])
-                continue;
-            dist[to] = distance;
-            pq.push({distance , to});
-        }
-    }
-    return dist;
-}
-
-
 class Solution {
 public:
+    typedef pair<int,int> p;
     vector<int> minimumTime(int n, vector<vector<int>>& edges, vector<int>& disappear) {
-        vector <int> ans = Dijktra(edges , 0 , n , disappear);
-        for(int &i : ans)
+        vector<vector<p>> adj(n);
+        vector <int> dist(n , INT_MAX);
+        for(auto e : edges)
+            adj[e[0]].push_back({e[2] , e[1]}),
+            adj[e[1]].push_back({e[2] , e[0]});
+        auto dijkstra = [&](int source){
+            dist[source] = 0;
+            priority_queue <p , vector<p> , greater<p>> pq;
+            pq.push({0 , source});
+            while(not pq.empty()){
+                auto [distance , curr] = pq.top(); pq.pop();
+                if(distance > dist[curr])
+                    continue;
+                for(auto &[t , next] : adj[curr]){
+                    int time = t + dist[curr];
+                    if(time >= dist[next] or time >= disappear[next])
+                        continue;
+                    dist[next] = time;
+                    pq.push({time , next});
+                }
+            }
+        };
+        dijkstra(0);
+        for(int &i : dist)
             if(i == INT_MAX)
                 i = -1;
-        return ans;
+        return dist;
     }
 };
-
-/*
-undirected edge, i'th node disappears at disappear[i], graph could be diconnected and have multiple edge. Edge all positive. Dijkstra? Simple dijkstra if distance < disappear[i] assign -1?
-*/
