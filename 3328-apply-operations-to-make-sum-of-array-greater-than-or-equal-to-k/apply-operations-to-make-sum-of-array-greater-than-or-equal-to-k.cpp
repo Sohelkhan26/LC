8c55b8316acbbda9d5204d85599f825af262b242
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-13 21:28:59 +0600

    Time: 0 ms (100.00%) | Memory: 8.2 MB (72.45%) - LeetSync

--- 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -5,10 +5,18 @@ public:
             return 0;
         int low = 1 , high = 1e5 , mid , ans = -1;
         auto isPossible = [&](int total){
-            for(int dup = total ; dup >= 1 ; dup--){
-                int incOp = (total - dup);
-                if(1ll * (incOp + 1) * (dup + 1) >= k)
-                    return true;
+            int l = 0, r = total;
+            while (l <= r) {
+                int dup = (l + r) / 2;
+// now we want to find the number "x" after some number of 1st operation
+// we have to find smallest number "x" s.t x * (dup + 1) >= k -> x = ceil(k / (dup + 1))
+                int x = (k + dup) / (dup + 1);  // same as ceil(k / (dup + 1))
+                int inc = x - 1;
+                if (inc + dup <= total) {
+                    return true;  // feasible
+                } else {
+                    l = dup + 1;  // too few duplications, try more
+                }
             }
             return false;
         };
@@ -27,5 +35,4 @@ Binary search over total operation.
 inc : number of 1st opeartion
 dup : number of second operation.
 total = (1 + inc) * (dup + 1) , initial number = 1 , use inc steps to make it "X" , now duplicate it "dup" times , now there is 1 original and "dup" duplicate values, resulting in "total"
-
 */
