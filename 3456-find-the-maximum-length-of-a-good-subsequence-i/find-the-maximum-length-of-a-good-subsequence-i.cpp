f0829bfa22c129fc6d0915a41197eaa30ec90486
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-18 13:59:19 +0600

    Time: 1219 ms (27.67%) | Memory: 51.1 MB (38.35%) - LeetSync

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.cpp
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.cpp
@@ -7,7 +7,7 @@ public:
         int ans = INT_MIN;
         if(dp[i][prev + 1][k] != -1)
             return dp[i][prev + 1][k];
-        ans = max(ans , solve(i + 1 , prev , k , nums));
+        ans = solve(i + 1 , prev , k , nums); // skip or don't start here condition
         if(prev == -1)
             ans = max(ans , 1 + solve(i + 1 , i , k , nums)); // start the seq here
         else if(nums[i] == nums[prev])
@@ -21,3 +21,13 @@ public:
         return solve(0 ,-1 , k , nums);
     }
 };
+
+/*
+We can include atmost k non-equal-adjacent value in the subsequence.
+So keep track of previous index. 
+About the start of the sequence. We shouldn't k-- for the starting element.
+So, three case:
+1. skip the current element.
+2. Check for start of the subsequence.
+3. Include i'th element depending on 'k'
+*/
