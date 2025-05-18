9df4504eb14bbee9746ab64294803c0c3b248b1e
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-18 17:41:08 +0600

    Time: 10 ms (80.52%) | Memory: 36 MB (62.77%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -2,14 +2,27 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size() , ans = 0;
+        map <vector <int> , int> mp;
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                int k = 0;
-                while(k < n and grid[i][k] == grid[k][j])
-                    k++;
-                ans += (k == n);
-            }
+            vector <int> col;
+            for(int j = 0 ; j < n ; j++)
+                col.push_back(grid[j][i]);
+            auto it = mp.find(col);
+            if(it != mp.end())
+                it -> second++;
+            else mp.emplace(move(col) , 1);
         }
+        for(const auto &row : grid)
+            ans += mp[row];
         return ans;
     }
 };
+
+/*
+This code is taken from votrubac.
+move() function doesn't copy the vector, rather it gives up ownership of the internal memory. After that col vector becomes empty.
+ 
+Note that we need to use a three map (ordered map) instead of hash map. For a vector, the three map stops matching when bumps into a discrepancy, while hash map process all elements first to compute a hash.
+
+Note how we optimize the memory usage here. To check if a row exists in map, we are using existing rows. That way, we do not build an entire vector only to find out we have a mismatch on the first element.
+*/
