9b8e1e40b0440b8b279f10d8390f1b8046acb218
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-25 22:46:12 +0600

    Time: 3 ms (93.80%) | Memory: 77.4 MB (91.37%) - LeetSync

--- 3613-maximize-amount-after-two-days-of-conversions/maximize-amount-after-two-days-of-conversions.cpp
+++ 3613-maximize-amount-after-two-days-of-conversions/maximize-amount-after-two-days-of-conversions.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    double maxAmount(string init , vector<vector<string>>& pairs1, vector<double>& rates1, vector<vector<string>>& pairs2, vector<double>& rates2) {
-        unordered_map <string,double> dist;
-        dist[init] = 1.0;
-        auto bellman = [&](vector<vector<string>> &pairs , vector<double> &rate){
-            for(int t = 0 ; t < pairs.size() ; t++){
-                for(int i = 0 ; i < pairs.size() ; i++){
+    double maxAmount(string init, vector<vector<string>>& pairs1, vector<double>& rates1, vector<vector<string>>& pairs2, vector<double>& rates2) {
+        unordered_map <string,double> dp;
+        dp[init] = 1.0;
+        auto solve = [&](vector<vector<string>>& pairs , vector<double> rates){
+            int n = pairs.size();
+            for(int t = 0 ; t < n ; t++){
+                for(int i = 0 ; i < n ; i++){
                     string from = pairs[i][0] , to = pairs[i][1];
-                    dist[to] = max(dist[to] , rate[i] * dist[from]);
-                    dist[from] = max(dist[from] , 1.0 / rate[i] * dist[to]);
+                    dp[to] = max(dp[to] , rates[i] * dp[from]);
+                    dp[from] = max(dp[from] , 1.0 / rates[i] * dp[to]);
                 }
             }
         };
-        bellman(pairs1 , rates1);
-        bellman(pairs2 , rates2);
-        return dist[init];
+        solve(pairs1 , rates1);
+        solve(pairs2 , rates2);
+        return dp[init];
     }
-}; 
+};
