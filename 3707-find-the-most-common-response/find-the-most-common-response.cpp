d267bdac42bd24ddaf53afacd3fec77a0b879515
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-25 19:30:47 +0600

    Time: 997 ms (97.70%) | Memory: 714.2 MB (5.00%) - LeetSync

--- 3707-find-the-most-common-response/find-the-most-common-response.cpp
+++ 3707-find-the-most-common-response/find-the-most-common-response.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+    int maxCount = 0;
+    string ans;
+    struct node{
+        unordered_map <char,node*> child;
+        int end;
+        node() : end(0){}
+    };
+    node* root;
+    void add(const string &s){
+        node* curr = root;
+        for(const char &c : s){
+            if(curr -> child[c] == nullptr)
+                curr -> child[c] = new node();
+            curr = curr -> child[c];
+        }
+        curr -> end++;
+        if(curr -> end > maxCount)
+            ans = s , maxCount = curr -> end;
+        else if(curr -> end == maxCount)
+            ans = min(ans , s);
+    }
     string findCommonResponse(vector<vector<string>>& responses) {
-        unordered_map <string,int> all;
+        root = new node();
         for(auto &r : responses){
             set <string> st(r.begin() , r.end());
             for(const string &s : st)
-                all[s]++;
+                add(s);
         }
-        vector <pair<int,string>> freq;
-        for(auto &[s , c] : all)    
-            freq.push_back({c , s});
-        sort(freq.begin() , freq.end() , [&](auto a , auto b){
-            return a.first == b.first ? a.second < b.second : a.first > b.first;
-        });
-        return freq[0].second;
+        return ans;
     }
 };
