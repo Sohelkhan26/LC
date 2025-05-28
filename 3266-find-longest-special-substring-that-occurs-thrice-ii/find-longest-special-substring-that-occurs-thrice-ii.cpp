647a96a13b996d18aeb7cdca6425906a81e16b11
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-29 03:48:01 +0600

    Time: 63 ms (82.88%) | Memory: 36.5 MB (86.54%) - LeetSync

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.cpp
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        int n = s.size() , ans = -1;
-        vector<int> maxLen(n);
-        maxLen[0] = 1;
-        for(int i = 1 ; i < n ; i++)
-            maxLen[i] = (s[i] == s[i - 1] ? maxLen[i - 1] + 1 : 1);
-        vector<vector<int>> freq(26);
-        for(int i = 0 ; i < n ; i++)
-            freq[s[i] - 'a'].push_back(maxLen[i]);
-        for(int i = 0 ; i < 26 ; i++){
-            sort(freq[i].rbegin() , freq[i].rend());
-            if(freq[i].size() > 2)
-                ans = max(ans , freq[i][2]);
+        int n = s.size() , low = 1 , high = n , mid , ans = -1;
+        auto valid = [&](int len){
+            unordered_map <char,int> count;
+            char last = '%';
+            int l = 1; 
+            for(char &c : s){
+                l = (c == last) ? l + 1 : 1;
+                last = c;
+                if(l >= len) // in case of aaaaa , len = 3, count will still increase for every a after "aaa".
+                    count[c]++;
+            }
+            for(auto &[c , cnt] : count)
+                if(cnt >= 3)
+                    return true;
+            return false;
+        };
+        while(low <= high){ // binary search over the max len
+            mid = low + (high - low) / 2;
+            if(valid(mid)) // check if any string with max len occurs at least 3 times.
+                ans = mid , low = mid + 1; // occurs, so try to increase max len.
+            else high = mid - 1; // no such string found , decrease max len.
         }
         return ans;
     }
