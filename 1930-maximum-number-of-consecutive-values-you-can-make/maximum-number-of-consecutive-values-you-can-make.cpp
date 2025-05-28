5cc3737bbcc176fa2df59f52d51f26c064ec77c1
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-28 22:20:05 +0600

    Time: 12 ms (97.92%) | Memory: 70.4 MB (81.25%) - LeetSync

--- 1930-maximum-number-of-consecutive-values-you-can-make/maximum-number-of-consecutive-values-you-can-make.cpp
+++ 1930-maximum-number-of-consecutive-values-you-can-make/maximum-number-of-consecutive-values-you-can-make.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int getMaximumConsecutive(vector<int>& coins) {
-        int canMake = 0;
+        int canMake = 1 , n = coins.size();
         sort(coins.begin() , coins.end());
-        for(int &coin : coins){
-            if(coin > canMake + 1)
-                return canMake + 1;
-            canMake += coin;
-        }
-        return canMake + 1;
+        for(int i = 0 ; i < n and coins[i] <= canMake ; i++)
+            canMake += coins[i];
+        return canMake;
     }
 };
+
+/*
+Like induction.
+If we can make number in range [0..k] , then after adding an number x we can make all the numbers up to x + k
+we can make [0..k] = [0 , ... , k - 3 , k - 2 , k - 1 , k] by adding them with previous values of k we can make numbers in range [x , x + k]
+We are required to find max consecutive integers we can make. Suppose we can make numbers in range [0..canMake] so, after we can make numbers in range [x..canMake + x] , in order to be consecutive x <= canMake + 1 otherwise there will be a gap.
+*/
