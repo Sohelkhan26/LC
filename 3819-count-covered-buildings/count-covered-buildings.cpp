fbc9274b4fb303122c8a2e5d6c58ca07c9a63d3f
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-29 15:12:31 +0600

    Time: 25 ms (94.83%) | Memory: 283.9 MB (90.58%) - LeetSync

---
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        vector <int> up(n + 1 , INT_MAX) , down(n + 1 , -1) , left(n + 1 , INT_MAX) , right(n + 1 , -1);
+        for(auto &it : buildings){
+            int x = it[0] , y = it[1];
+            up[x] = min(up[x] , y); // store top-most building at position x
+            down[x] = max(down[x] , y);
+            left[y] = min(left[y] , x); // store left-most building at position y
+            right[y] = max(right[y] , x);
+        } 
+        int ans = 0;
+        for(auto &it : buildings){
+            int x = it[0] , y = it[1];
+            if(x > left[y] and x < right[y] and y > up[x] and y < down[x])
+                ans++;
+        }
+        return ans;
+    }
+};
