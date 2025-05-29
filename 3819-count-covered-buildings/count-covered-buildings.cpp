89b5d37c41e2afb58020b04a3228e28e6f5eb44b
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-29 15:16:22 +0600

    Time: 25 ms (94.83%) | Memory: 283.8 MB (95.31%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.cpp
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -4,9 +4,9 @@ public:
         vector <int> up(n + 1 , INT_MAX) , down(n + 1 , -1) , left(n + 1 , INT_MAX) , right(n + 1 , -1);
         for(auto &it : buildings){
             int x = it[0] , y = it[1];
-            up[x] = min(up[x] , y); // store top-most building at position x
+            up[x] = min(up[x] , y); // store top-most building at position x(vertically)
             down[x] = max(down[x] , y);
-            left[y] = min(left[y] , x); // store left-most building at position y
+            left[y] = min(left[y] , x); // store left-most building at position y(horizontally)
             right[y] = max(right[y] , x);
         } 
         int ans = 0;
@@ -18,3 +18,8 @@ public:
         return ans;
     }
 };
+
+/*
+left[y] means left-most building at height  y(along the horizontal line)
+up[x] means top-most building at width x(along the vertical line)
+*/
