fbd00e0fdaa76b6d34034e0fa3a4fa1b4acb6956
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-02 01:17:57 +0600

    Time: 962 ms (87.74%) | Memory: 267.2 MB (65.09%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int maximizeSquareArea(int m, int n, vector<int>& hor, vector<int>& ver) {
-        hor.emplace_back(1);
-        hor.emplace_back(m);
-        ver.emplace_back(1);
-        ver.emplace_back(n);
-        unordered_map <int,bool> diff;
-        for(int i = 0 ; i < hor.size() ; i++)
-            for(int j = 0 ; j < hor.size() ; j++)
-                diff[abs(hor[j] - hor[i])] = true;
-        
-        int ans = 0 , mod = 1e9 + 7;
-        for(int i = 0 ; i < ver.size() ; i++)
-            for(int j = 0 ; j < ver.size() ; j++){
-                int d = abs(ver[j] - ver[i]);
-                if(i != j and diff.contains(d))
-                    ans = max(ans , d);
+    int solve(int n , vector<int> &a , unordered_set<int> &diff){
+        int ans = -1;
+        a.emplace_back(1);
+        a.emplace_back(n);
+        bool verify = not diff.empty();
+        for(int i = 0 ; i < a.size() ; i++){
+            for(int j = i + 1 ; j < a.size() ; j++){
+                int d = abs(a[j] - a[i]);
+                if(verify){
+                    if(diff.contains(d))
+                        ans = max(ans , d);
+                }else diff.insert(d);
             }
-        return ans == 0 ? -1 : (1ll * ans * ans) % mod;
-    }           
+        }
+        return ans;
+    }
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        unordered_set <int> diff;
+        solve(m , hFences , diff);
+        int ans = solve(n , vFences , diff) , mod = 1e9 + 7;
+        return ans != -1 ? (1ll * ans * ans) % mod : ans;
+    }
 };
