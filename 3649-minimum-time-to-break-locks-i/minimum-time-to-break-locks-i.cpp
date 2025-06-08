7b7205ca203446a6d7086bbcb417ea02412442b5
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-09 01:27:50 +0600

    Time: 192 ms (58.61%) | Memory: 25.2 MB (54.21%) - LeetSync

--- 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int findMinimumTime(vector<int>& strength, int k) {
-        int n = strength.size();
-        function<int(int,int)> solve = [&](int mask , int x){
-            if(mask == (1 << n) - 1)
-                return 0;
-            int cost = 1e9;
-            for(int i = 0 ; i < n ; i++){ // open the i'th lock
-                if(mask & (1 << i))
-                    continue;
-                cost = min(cost , (int)ceil((strength[i] * 1.0) / x) + solve(mask | (1<<i) , x + k));
-            }
-            return cost;
-        };  
-        return solve(0 , 1);
+    int findMinimumTime(vector<int>& s , int k) {
+        sort(s.begin() , s.end());
+        int ans = INT_MAX;
+        do{
+            int minute = 0 , x = 1;
+            for(int &i : s)
+                minute += (i + x - 1) / x , x += k;
+            ans = min(ans , minute);
+        }while(next_permutation(s.begin() , s.end())); // use do-while for TC s = [3]
+        return ans;
     }
 };
-/*
-Tried Binary search with greedy. don't work.
-order of picking which lock to open effects the total time.
-constraint is low, try all possible combination. at time i, 
-consider opening any lock out of n, solve the subproblem and return the min.
-*/
