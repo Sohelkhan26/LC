f23cd7608836db8321a9486430a83f4e59c52123
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-12 22:20:30 +0600

    Time: 103 ms (65.39%) | Memory: 93.1 MB (8.09%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int ans = 0;
-        for(int i = 0 ; i < 30 ; i++){
-            int bit = 0;
-            for(int &n : nums)
-                bit ^= ((n >> i) & 1);
-            ans += (bit xor ((k >> i) & 1));
-        }
-        return ans;
+        return __builtin_popcount(accumulate(nums.begin() , nums.end() , 0 , [&](int sum , int n){return sum xor n;}) xor k);
     }
 };
