d02a34a9023a88a8d7ced3183622a1b8db14dcde
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-15 15:51:20 +0600

    Time: 0 ms (100.00%) | Memory: 9.3 MB (16.11%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int maxDiff(int num) {
-        string s = to_string(num);
-        int ans = -1 , n = s.size() , mn = INT_MAX , mx = INT_MIN; 
-        for(char c = '0' ; c <= '9' ; c++){
-            for(char d = '0' ; d <= '9' ; d++){
-                for(char &a : s)
-                    if(a == c)
-                        a = d;
-                if(s[0] == '0'){
-                    s = to_string(num);
-                    continue;
-                }
-                mx = max(mx , stoi(s));
-                mn = min(mn , stoi(s));
-                s = to_string(num);                
+        string origin = to_string(num);
+        int mx = INT_MIN , mn = INT_MAX;
+        for(char o = '0' ; o <= '9' ; o++){
+            for(char n = '0' ; n <= '9' ; n++){
+                string mod = origin;
+                for(char &c : mod)
+                    if(c == o)
+                        c = n;
+                if(mod[0] == '0')continue;
+                int val = stoi(mod);
+                mx = max(mx , val);
+                mn = min(mn , val);
             }
-        } 
+        }
         return mx - mn;
     }
 };
