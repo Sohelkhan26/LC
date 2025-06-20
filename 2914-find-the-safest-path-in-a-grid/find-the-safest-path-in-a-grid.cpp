7e873a3d624aa0f519d452e0d7413189473ba08b
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-20 17:21:12 +0600

    Time: 633 ms (23.66%) | Memory: 203.7 MB (34.87%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,56 +1,62 @@
 class Solution {
 public:
-    typedef pair<int,int> P;
-    
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int n = grid.size() , m = grid[0].size();
 
         vector<vector<int>> dist(n , vector<int> (m , INT_MAX));
-        // dist[i][j] distance to nearest thief
-        queue <P> q;
-
-        if(grid[0][0] == 1 or grid[n - 1][m - 1] == 1)
-            return 0; 
-
+        auto good = [&](int i , int j){
+            return min(i , j) >= 0 and i < n and j < m;
+        };
+        queue <pair<int,int>> q;
+        int path[6] = {1 , 0 , -1 , 0 , 1};
         for(int i = 0 ; i < n ; i++)
             for(int j = 0 ; j < m ; j++)
                 if(grid[i][j])
                     q.push({i , j}) , dist[i][j] = 0;
 
-        auto valid = [&](int i , int j){
-            return min(i , j) >= 0 and i < n and j < m;
-        };
-
-        int path[6] = {1 , 0 , -1 , 0 , 1};
         while(not q.empty()){
             int len = q.size();
             while(len--){
                 auto [i , j] = q.front(); q.pop();
                 for(int k = 0 ; k < 4 ; k++){
                     int newI = i + path[k] , newJ = j + path[k + 1];
-                    if(valid(newI , newJ) and dist[newI][newJ] == INT_MAX){
+                    if(good(newI , newJ) and dist[newI][newJ] == INT_MAX){
                         q.push({newI , newJ});
                         dist[newI][newJ] = dist[i][j] + 1;
                     }
                 }
             }
         }
-        priority_queue <array<int,3>> pq; // find the farthest path from any thief
-        pq.push({dist[0][0] , 0 , 0}); // min(dist[i][j]) along the path is max
-        vector<vector<bool>> vis(n , vector<bool> (m));
-        while(not pq.empty()){
-            auto t = pq.top() ; pq.pop();
-            int d = t[0] , i = t[1] , j = t[2];
-            if(i == n - 1 and j == m - 1)
-                return d;
-            for(int k = 0 ; k < 4 ; k++){
-                int newI = i + path[k] , newJ = j + path[k + 1];
-                if(valid(newI , newJ) and not vis[newI][newJ]){
-                    pq.push({min(d , dist[newI][newJ]) , newI , newJ});
-                    vis[newI][newJ] = true;
+        auto valid = [&](int mid){ // try to find a path such that min(dist[i][j]) along the path <= mid
+            queue <pair<int,int>> q;
+            vector<vector<bool>> vis(n , vector<bool> (m));
+            q.push({0 , 0});
+            if(dist[0][0] < mid)
+                return false;
+            while(not q.empty()){
+                int len = q.size();
+                while(len--){
+                    auto [i , j] = q.front(); q.pop();
+                    if(i == n - 1 and j == m - 1)
+                        return true;
+                    for(int k = 0 ; k < 4 ; k++){
+                        int newI = i + path[k] , newJ = j + path[k + 1];
+                        if(good(newI , newJ) and vis[newI][newJ] == false and dist[newI][newJ] >= mid){
+                            q.push({newI , newJ});
+                            vis[newI][newJ] = true;
+                        }
+                    }
                 }
             }
+            return false;
+        };
+        int low = 0 , high = max(n , m) , ans = -1 , mid;
+        while(low <= high){ // binary search over max safeness factor
+            mid = low + (high - low) / 2;
+            if(valid(mid))
+                ans = mid , low = mid + 1;
+            else high = mid - 1;
         }
-        return 0;
+        return ans;
     }
 };
