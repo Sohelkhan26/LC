16906284ebcc9333cf481b37ec052db91173f775
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-23 20:54:43 +0600

    Time: 380 ms (82.78%) | Memory: 385.2 MB (64.92%) - LeetSync

--- 3842-number-of-ways-to-assign-edge-weights-ii/number-of-ways-to-assign-edge-weights-ii.cpp
+++ 3842-number-of-ways-to-assign-edge-weights-ii/number-of-ways-to-assign-edge-weights-ii.cpp
@@ -35,7 +35,7 @@ public:
         
         int diff = level[a] - level[b];
         for (int i = height; i >= 0; --i)
-            if ((diff >> i) & 1)
+            if (diff & (1 << i))
                 a = up[a][i];
 
         if(a == b)
@@ -54,7 +54,7 @@ public:
     }
 
     vector<int> assignEdgeWeights(vector<vector<int>>& edges, vector<vector<int>>& queries) {
-        n = edges.size() + 1;
+        n = edges.size() + 1; // tree , so edges.size() = n - 1
         height = ceil(log2(n));
 
         adj = vector<vector<int>> (n + 1);
@@ -65,7 +65,7 @@ public:
             adj[e[0]].push_back(e[1]),
             adj[e[1]].push_back(e[0]);
 
-        dfs(1 , -1 , 0);
+        dfs(1 , -1 , 0); // call dfs before binary lifting to assign 1st parent to each node
         binaryLifting();
 
         int m = queries.size();
@@ -80,3 +80,32 @@ public:
         return ans;
     }
 };
+
+/*
+all the info about tree don't matter.
+find number of ways to assign weight to path between query[i] = [u , v] such that cost-path is odd.
+
+Maybe combinatorics + DP problem?
+if there are 'n' nodes along the path , n - 1 edges to assign.
+i = 1,3,5... edges should have 1 weight
+and other can have 0/2 weight to make the path cost odd.
+We have to find an efficient way to calculate this.
+for(i = 1 ; i < n ; i += 2)
+    n - 1 C i * pow(2 , n - 1 - i) ways?
+
+I was wrong, we can't leave any edge as 0
+if a path has n edges:
+Total number ways to assign = 2 ^ n (either 1 or 2)
+out of them half will have weight sum as odd
+so 2 ^ (n - 1) ways if n > 1
+This problem took so fucking time. Nodes are 1 based, it annoyed me too much. The problem was hard enough and adding these fucking details just annoyed me. Otherwise it was fun.
+
+This is a combination of some problems.
+DFS + Binary Lifting + Finding LCA + combinatorics(minimum)
+
+For LCA watch Erichto's : https://www.youtube.com/watch?v=dOAxrhAUIhA&ab_channel=ErrichtoAlgorithms
+
+Sub-problem:
+https://cses.fi/problemset/task/1135 
+
+*/
