f56ef95396d365abd134e02a3fcd0ce5efb4f461
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-01 02:33:00 +0600

    Time: 2710 ms (5.23%) | Memory: 27.5 MB (29.22%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
@@ -4,23 +4,13 @@ public:
         int n = nums.size() , ans = 0;
         vector <int> pref = {nums[0] > 8 ? 1 : -1};
         for(int i = 1 ; i < n ; i++)
-            pref.push_back(pref.back() + (nums[i] > 8 ? 1 : -1));
-        for(int i : pref)
-            cout << i << ' ';
+            pref.push_back(pref.back() + (nums[i] > 8 ? 1 : -1)); 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
                 if(pref[j] - (i > 0 ? pref[i - 1] : 0) > 0)
                     ans = max(ans , j - i + 1);
             }
-        }
-        // for(int i = 0 ; i < n ; i++)
-        //     pref.push_back(pref.back() + (nums[i] > 8 ? 1 : -1));
-        // for(int i = 1 ; i <= n ; i++){
-        //     for(int j = i + 1 ; j <= n ; j++){
-        //         if(pref[j] - pref[i] > 0)
-        //             ans = max(ans , j - i + 1);
-        //     }
-        // }
+        } 
         return ans;
     }
 };
@@ -33,4 +23,5 @@ Sliding window seems too easy, right?
 Yeah, typical one don't work.
 Kadene won't work too. Prefix sum sliding window maybe?
 Prefix sum with two loops should pass?
+Indeed passed cause of low constraints. But barely. 2.2 seconds
 */
