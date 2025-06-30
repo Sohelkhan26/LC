6a574791518e694c780fcfa7b0419552ff0431ed
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-01 01:03:51 +0600

    Time: 11 ms (65.91%) | Memory: 15.6 MB (53.69%) - LeetSync

--- 1239-largest-1-bordered-square/largest-1-bordered-square.cpp
+++ 1239-largest-1-bordered-square/largest-1-bordered-square.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int largest1BorderedSquare(vector<vector<int>>& grid) {
         int n = grid.size() , m = grid[0].size() , ans = 0;
-        for(int i = 0 ; i < n ; i++){ 
-            for(int j = 0 ; j < m ; j++){ 
-                for(int len = 1 ; i + len <= n and j + len <= m ; len++){
-                    bool ok = true;
-                    for(int k = 0 ; k < len ; k++){
-                        if(grid[i][j + k] == 0) // first row
-                            ok = false;
-                        if(grid[i + len - 1][j + k] == 0) // last row
-                            ok = false;
-                        if(grid[i + k][j] == 0) // first col
-                            ok = false;
-                        if(grid[i + k][j + len - 1] == 0) // last col
-                            ok = false;
-                    }
-                    // for(int k = 0 ; k < len ; k++){
-                    // }
-                    if(ok)
-                        ans = max(ans , len * len);
+        vector <vector<int>> right(n + 1 , vector <int> (m + 1)) , down;
+        down = right;
+        for(int i = n - 1 ; i >= 0 ; i--)
+            for(int j = m - 1 ; j >= 0 ; j--)
+                if(grid[i][j]){
+                    right[i][j] = 1 + right[i][j + 1];
+                    down[i][j] = 1 + down[i + 1][j];
                 }
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                int maxSize = min(right[i][j] , down[i][j]);
+                for(int side = maxSize ; side > 0 ; side--)
+                    if(right[i + side - 1][j] >= side and down[i][j + side - 1] >= side){
+                        ans = max(ans , side * side);
+                        break;
+                    }
             }
         }
         return ans;
