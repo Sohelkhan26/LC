755c4f8e4b0d4eb7882c3c7e4f127d0989ace320
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-01 02:29:40 +0600

    Time: 2772 ms (5.23%) | Memory: 27.2 MB (47.98%) - LeetSync

---
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestWPI(vector<int>& nums) {
+        int n = nums.size() , ans = 0;
+        vector <int> pref = {nums[0] > 8 ? 1 : -1};
+        for(int i = 1 ; i < n ; i++)
+            pref.push_back(pref.back() + (nums[i] > 8 ? 1 : -1));
+        for(int i : pref)
+            cout << i << ' ';
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(pref[j] - (i > 0 ? pref[i - 1] : 0) > 0)
+                    ans = max(ans , j - i + 1);
+            }
+        }
+        // for(int i = 0 ; i < n ; i++)
+        //     pref.push_back(pref.back() + (nums[i] > 8 ? 1 : -1));
+        // for(int i = 1 ; i <= n ; i++){
+        //     for(int j = i + 1 ; j <= n ; j++){
+        //         if(pref[j] - pref[i] > 0)
+        //             ans = max(ans , j - i + 1);
+        //     }
+        // }
+        return ans;
+    }
+};
+/*
+Interval is consecutive days?
+elements >= 9 -> 1
+others, -> -1?
+find len of longest subarray such that sum of subarray >= 1? Kadene? if subarray sum < 0 discard? Or sliding window? shrink when sum <= 0
+Sliding window seems too easy, right?
+Yeah, typical one don't work.
+Kadene won't work too. Prefix sum sliding window maybe?
+Prefix sum with two loops should pass?
+*/
