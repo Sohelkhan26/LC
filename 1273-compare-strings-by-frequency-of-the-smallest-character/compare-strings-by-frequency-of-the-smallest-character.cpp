64add73e733befc588ba5ae098eb7b2e41b942ad
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-02 18:12:20 +0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (25.39%) - LeetSync

--- 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -9,15 +9,21 @@ public:
                 if(i > 0)
                     return i;
             return -1;
-        };
-        vector <int> freq , ans;
+        }; 
+        vector <int> ans(11) , result;
         for(string &s : words)
-            freq.push_back(f(s));
-        sort(freq.begin() , freq.end());
-        for(string &s : queries){
-            int target = f(s); 
-            ans.push_back(freq.end() - upper_bound(freq.begin() , freq.end() , target));
-        }
-        return ans;
+            for(int i = f(s) - 1 ; i >= 0 ; i--) // this words will be counted for f(query) < f(s)
+                ans[i]++;
+        for(string &s : queries)
+            result.push_back(ans[f(s)]);
+        return result;
     }
 };
+
+/*
+Binary search is easy. O(n) is harder.
+each string length is at most 11
+So, f(words) can be at most 11
+We can calculate answer for each possible query.
+ans[j] stores count(words[i]) such that f(words[i]) > j
+*/
