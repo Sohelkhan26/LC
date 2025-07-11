e762dc3b088d77a8e9cc97624aa48d9dbe84f43d
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-12 02:37:21 +0600

    Time: 0 ms (100.00%) | Memory: 27.3 MB (78.26%) - LeetSync

--- 1739-split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.cpp
+++ 1739-split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.cpp
@@ -17,3 +17,16 @@ public:
         return check(a , b) or check(b , a);
     }
 };
+
+/*
+Given two strings, fix a index and split both strings at that index
+Find if pref(a) + suff(b) or pref(b) + suff(a) forms palindrome or not.
+Match prefix and suffix of both strings as long as possible.
+After that if middle part of any string is palindrome, we can add that to 
+that string's prefix/suffix and split there. (do this for a|b and b|a)
+a = "abcxcxef"
+b = "efgeaoga"
+ef match for both string, left = 1 , right = 6
+Middle part of string 'a' is palindrome, so we can add that to the suffix of 'a'
+So, we can split both at index 1
+*/
