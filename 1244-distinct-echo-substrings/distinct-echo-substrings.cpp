b26e96b49343fe310197efa19074f0d1b35620a1
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-13 00:50:51 +0600

    Time: 1883 ms (8.24%) | Memory: 9.4 MB (97.42%) - LeetSync

--- 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
+++ 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int distinctEchoSubstrings(string text) {
-        unordered_set <string> ans;
+        unordered_set <int> ans;
+        auto hash = [&](int left , int right){
+            long long base = 109 , mod = 1e9 + 9 , Hash = 0;
+            for(int i = left ; i < right ; i++)
+                Hash = (Hash * base + (text[i] - 'a' + 1)) % mod;
+            return Hash;
+        };
         int n = text.size();
         for(int len = 1 ; len <= n / 2 ; len++){
             int count = 0;
             for(int left = 0 , right = len ; left + len < n ; left++ , right++){
                 count = (text[left] == text[right] ? count + 1 : 0);
                 if(count == len){
-                    ans.insert(text.substr(left , len));
-                    count--;
+                    ans.insert(hash(left , left + len));
+                    count--; // not count = 0, think for aaaa
                 }
             }
         }
