597963bc1d27ad6e4d9213a5be9d7e6735b1f573
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-14 00:24:45 +0600

    Time: 6 ms (94.30%) | Memory: 43.2 MB (95.34%) - LeetSync

--- 2739-minimize-the-total-price-of-the-trips/minimize-the-total-price-of-the-trips.cpp
+++ 2739-minimize-the-total-price-of-the-trips/minimize-the-total-price-of-the-trips.cpp
@@ -1,68 +1,53 @@
 class Solution {
-public:
-    int n , totalCost = 0;
+public: 
     vector<vector<int>> adj;
     vector<int> contrib;
-
-    int dp[51][51][2];
+ 
 
     bool dfs(int curr , int par , int dest , vector<int> &price){ // DFS will return true along a path
-        if(curr == dest)
+        if(curr == dest){
+            contrib[dest]++;
             return true;
+        }
             
-        for(int nei : adj[curr])
-            if(nei != par and dfs(nei , curr , dest , price)){
-                totalCost += price[nei];
-                contrib[nei]++;
+        for(int nei : adj[curr]){
+            if(nei == par)
+                continue;
+            if(dfs(nei , curr , dest , price)){
+                contrib[curr]++;
                 return true;
             }
+        }
         return false;
     }
-
-    int solve(int curr , int par , bool canHalve , vector<int> &price){
-        if(dp[curr][par][canHalve] != -1)
-            return dp[curr][par][canHalve];
-
-        int ans = 0;
-        if(canHalve) // since calculating only discount
-            ans += (price[curr] / 2) * contrib[curr];
+    // {current node halved , current node not halved/skipped}
+    pair<int,int> solve(int curr , int par , vector<int> &price){
+        int take = (price[curr] / 2) * contrib[curr];
+        int skip = price[curr] * contrib[curr];
         for(int nei : adj[curr]){
-            if(nei != par){
-                int a = 0 , b = 0 , c = 0;
-                if(canHalve) // if we half this node, can't halve children
-                    a = solve(nei , curr , false , price);
-                else // if we don't half this node, we can half/skip childrens
-                    b = solve(nei , curr , false , price) , c = solve(nei , curr , true , price);
-                ans += max(a , max(b , c)); // take the maximum path
-            }
+            if(nei == par)
+                continue;
+            auto child = solve(nei , curr , price);
+            skip += min(child.first , child.second); // current node skipped, child can either be halve or skipped
+            take += child.second; // But if current node is halved, child node must be skipped
         }
-        return dp[curr][par][canHalve] = ans;
+        return {take , skip};
     }
-    int minimumTotalPrice(int N, vector<vector<int>>& edges, vector<int>& price, vector<vector<int>>& trips) {
-        n = N;
-
+    int minimumTotalPrice(int n, vector<vector<int>>& edges, vector<int>& price, vector<vector<int>>& trips) {
         adj = vector<vector<int>> (n);
-        contrib = vector<int> (n);
-        memset(dp , -1 , sizeof(dp));
+        contrib = vector<int> (n); 
 
         for(auto &e : edges)
             adj[e[0]].push_back(e[1]),
             adj[e[1]].push_back(e[0]); 
 
         for(auto &t : trips){
-            int start = t[0] , end = t[1]; 
-            totalCost += price[start]; // structure of DFS excludes the starting node
-            contrib[start]++; // so calculate it seperately
+            int start = t[0] , end = t[1];  
             dfs(start , start , end , price);
         }
 
-        int ans = totalCost;
-
-        for(int i = 0 ; i < n ; i++){
-            ans = min(ans , totalCost - solve(i , i , true , price));
-            ans = min(ans , totalCost - solve(i , i , true , price));
-        }
-        return ans;
+        auto ans = solve(0 , 0 , price);
+        return min(ans.first , ans.second);
     }
 };
 
