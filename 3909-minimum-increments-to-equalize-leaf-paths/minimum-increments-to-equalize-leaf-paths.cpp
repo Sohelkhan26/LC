93f8c5e4a6874e0097afac6cc21878b3a03d23f8
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-14 23:59:48 +0600

    Time: 248 ms (61.19%) | Memory: 306.3 MB (59.88%) - LeetSync

--- 3909-minimum-increments-to-equalize-leaf-paths/minimum-increments-to-equalize-leaf-paths.cpp
+++ 3909-minimum-increments-to-equalize-leaf-paths/minimum-increments-to-equalize-leaf-paths.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int ans = 0;
+    typedef long long ll;
     vector<vector<int>> adj;
-    long long dfs(int curr , int par , vector<int> &price){
-        long long maxCost = 0;
-        vector <long long> child;
-        for(int nei : adj[curr]){
-            if(nei == par)
-                continue;
-            long long t = dfs(nei , curr , price);
-            maxCost = max(maxCost , t);
-            child.push_back(t);
+    pair<long long,int> dfs(int curr , int par , vector<int> &cost){
+        vector <ll> childs;
+        int ans = 0;
+        for(int &nei : adj[curr]){
+            if(nei == par) continue;
+            auto childAns = dfs(nei , curr , cost);
+            ans += childAns.second;
+            childs.push_back(childAns.first);
         }
-        for(long long &i : child) // in a tree, one node can have multiple child
-            if(i != maxCost) // out of all the child path values, if they don't match the max child path value, one node should be increased
+        if(childs.empty()) // leaf node
+            return {cost[curr] , 0};
+        long long maxChild = *max_element(childs.begin() , childs.end());
+        for(auto &c : childs)
+            if(c != maxChild)
                 ans++;
-        return 0ll + price[curr] + maxCost;
+        return {maxChild + cost[curr] , ans};
     }
     int minIncrease(int n, vector<vector<int>>& edges, vector<int>& cost) {
         adj.resize(n);
         for(auto &e : edges)
             adj[e[0]].push_back(e[1]),
             adj[e[1]].push_back(e[0]);
-        dfs(0 , 0 , cost);
-        return ans;
+        return dfs(0 , 0 , cost).second;
     }
 };
 
