2966f8f8b0106fa21fa695b807631be3fe1a70e7
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-17 03:47:50 +0600

    Time: 147 ms (62.88%) | Memory: 229.8 MB (10.84%) - LeetSync

--- 472-concatenated-words/concatenated-words.cpp
+++ 472-concatenated-words/concatenated-words.cpp
@@ -1,25 +1,63 @@
 class Solution {
 public:
+    struct Node {
+        bool isEnd = false;
+        Node* child[26] = {};
+    }* root = new Node();
+
+    // Resuable dp array
+    bool dp[31];
+
+    // Insert word into trie
+    void add(string& s) {
+        Node* curr = root;
+        for (char c : s) {
+            int idx = c - 'a';
+            if (!curr->child[idx])
+                curr->child[idx] = new Node();
+            curr = curr->child[idx];
+        }
+        curr->isEnd = true;
+    }
+
+    // For a word, update dp for all possible break points
+    void updateDp(Node* node, string& word, int start) {
+        for (int i = start; i < word.size(); ++i) {
+            int idx = word[i] - 'a';
+            node = node->child[idx];
+            if (!node)
+                break;
+            if (node->isEnd) {
+                dp[i + 1] = true;
+            }
+        }
+    }
+
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        unordered_set <string> wordSet(words.begin() , words.end());
-        
+        // Sort by length
+        sort(words.begin(), words.end(), [](const string& a, const string& b) {
+            return a.size() < b.size();
+        });
+
         vector<string> ans;
-    
-        for (string &word : words) {
-            int n = word.size();
-            vector<bool> dp(n + 1, 0);
+        for (string& word : words) {
+            if (word.empty())
+                continue;
+            // Reset DP for this word
+            memset(dp, false, sizeof(bool) * (word.size() + 1));
             dp[0] = true;
-            for(int i = 0 ; i < n ; i++){
-                if(not dp[i])continue;
-                for(int len = i + 1 ; len <= n ; len++){
-                    if(len - i < n and wordSet.count(word.substr(i , len - i)))
-                        dp[len] = true;
-                }
-                if(dp[n]){
-                    ans.push_back(word);
-                    break;
+            // Update DP via trie
+            for (int i = 0; i < word.size(); ++i) {
+                if (dp[i]) {
+                    updateDp(root, word, i);
+                    if (dp[word.size()]) {
+                        ans.push_back(word);
+                        break;
+                    }
                 }
             }
+            // Add current word to trie for future words
+            add(word);
         }
         return ans;
     }
