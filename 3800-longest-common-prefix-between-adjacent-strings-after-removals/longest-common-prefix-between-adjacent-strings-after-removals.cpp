7176b18fe91db57f1b7c9bf1c60f2685f6387aa3
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-20 02:47:55 +0600

    Time: 96 ms (25.56%) | Memory: 201.4 MB (94.59%) - LeetSync

--- 3800-longest-common-prefix-between-adjacent-strings-after-removals/longest-common-prefix-between-adjacent-strings-after-removals.cpp
+++ 3800-longest-common-prefix-between-adjacent-strings-after-removals/longest-common-prefix-between-adjacent-strings-after-removals.cpp
@@ -7,16 +7,16 @@ public:
                 ans++;
             return ans;
         };
-        unordered_map <int,int> freq;
-        multiset <int> all;
+        unordered_map <int,int> freq; // freq of prefix-length
+        multiset <int> all; // only prefix length for faster query
 
         auto add = [&](int len){
-            if(++freq[len] == 1)
+            if(++freq[len] == 1) // this prefix length is 1st
                 all.insert(len);
         };
 
         auto remove = [&](int len){
-            if(--freq[len] == 0){
+            if(--freq[len] == 0){ // this prefix length is the last
                 freq.erase(len);
                 all.erase(len);
             }
@@ -36,12 +36,15 @@ public:
                 len2 = solve(words[i] , words[i + 1]);
             if(i - 1 >= 0 and i + 1 < n)
                 len3 = solve(words[i - 1] , words[i + 1]);
-            remove(len1);
-            remove(len2);
-            add(len3);
+            remove(len1) , remove(len2) , add(len3);
             ans.push_back(all.empty() ? 0 : *all.rbegin());
             add(len1) , add(len2) , remove(len3);
         }
         return ans;
     }
 };
+
+/*
+One important observation is , after removing i'th word, All adjacent pairs don't change. Only (i - 1 , i) and (i , i + 1) prefix get removed(if they exist) and (i - 1 , i + 1) new adjacent prefix get added and all other's relative order stays the same. So we can precompute common prefix for all pairs.
+For each index, we can simulate the answer.
+*/
