4dea724041489fe77da7fd60ac49be248da0599b
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-22 14:25:38 +0600

    Time: 148 ms (57.76%) | Memory: 118.2 MB (50.60%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int ans = 0 , n = nums.size() , window = 0;
-        unordered_map <int , int> count;
+        int n = nums.size() , ans = 0 , window = 0;
+        unordered_map <int,int> freq;
         for(int left = 0 , right = 0 ; right < n ; right++){
-            if(count.contains(nums[right])){
-                while(left <= count[nums[right]]){
-                    count.erase(nums[left]);
-                    window -= nums[left++];
-                }
-            }
-            count[nums[right]] = right;
+            freq[nums[right]]++;
             window += nums[right];
+            while(freq[nums[right]] > 1){
+                window -= nums[left];
+                if(--freq[nums[left]] == 0)
+                    freq.erase(nums[left]);
+                left++;
+            }
             ans = max(ans , window);
         }
         return ans;
