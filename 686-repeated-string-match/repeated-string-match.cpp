1c988a57225d39e7a22c1bbfa110a5facb77629c
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-24 02:52:56 +0600

    Time: 0 ms (100.00%) | Memory: 9 MB (97.88%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -7,17 +7,23 @@ public:
             bHash = (bHash * base + (c - 'a' + 1)) % mod;
             power = (power * base) % mod;
         }
-        for(int i = 0 ; i < 2 * m + n ; i++){
+        for(int i = 0 ; i < max(2 * m , 2 * n) ; i++){
             int j = i % n;
             aHash = (aHash * base + (a[j] - 'a' + 1)) % mod;
             if((i + 1) > m)
-                aHash = (aHash - (1LL * (a[(i - m) % n] - 'a' + 1) * power) % mod) % mod;
+                aHash = (aHash - (1LL * (a[(i - m) % n] - 'a' + 1) * power) % mod) % mod; // don't forget to mod index i by n
             if(aHash < 0)
                 aHash += mod;
 
             if(aHash == bHash) 
-                return (i + 1 + n - 1) / n;
+                return (i + 1 + n - 1) / n; // shortcut for ceil(1.0 * (i + 1) / n) 
         }
         return -1;
     }
 };
+
+/*
+Given two strings, return min times 'a' need to be appended to itself such that 'b' is a substring in 'a'.
+Instead of appending 'a' to itself until it's size exceeds b.size() we can simulate this process by looping from [0..2 * m + n] 
+And keep a 'm' size window to check if they match. Variables are taken such that collision don't occur. In case of collision, do manual checking. But this one passes just fine.
+*/
