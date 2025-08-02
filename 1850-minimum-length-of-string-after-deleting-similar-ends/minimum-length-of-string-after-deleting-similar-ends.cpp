ecb29e394c65d68a4bbe14cc89432acb27aa1a31
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-08-02 19:14:05 +0600

    Time: 0 ms (100.00%) | Memory: 16 MB (32.50%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int solve(const string &s , int left , int right){  
-        if(left == right)
-            return 1;
-        char a = s[left] , b = s[right];
-        if(a != b)
-            return right - left + 1;
-        while(a == s[left] and left < right)
-            left++;
-        if(left == right)
-            return 0;
-        while(b == s[right] and left < right)
-            right--;
-        return solve(s , left , right);
-    }
     int minimumLength(string s) {
-        return solve(s , 0 , s.size() - 1);
+        int left = 0 , n = s.size() , right = n - 1;
+        while(left < right and s[left] == s[right]){
+            char c = s[left];
+            while(left <= right and s[left] == c)
+                left++;
+            while(left <= right and s[right] == c)
+                right--;
+        }
+        return right - left + 1;
     }
 };
